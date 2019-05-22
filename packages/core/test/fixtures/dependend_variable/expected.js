5d74d9cd261e871d81b8e92f08da31ff72ffdd87
Simon Siefke <simon.siefke@gmail.com>
2019-05-23 00:25:05 +0200

    docs: update mechanism

--- packages/core/test/fixtures/dependend_variable/expected.js
+++ packages/core/test/fixtures/dependend_variable/expected.js
@@ -1,7 +1,8 @@
 import { updates, invalidate, dirty } from 'ui'
 
-let x = 1
+let x = 0
 let y = x + 1
+x =  22; invalidate('x');
 
 function update(){
   if(dirty.has('x')){y = x + 1;invalidate('y')}
